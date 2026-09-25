8b7b4e1765340b1c4aafbab084c15b53f841cff9
shruti7611 <itshrutii@gmail.com>
2026-09-25 23:14:12 +0530

    Update AboutUs.jsx

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,1 +1,34 @@
+import React from "react";
 
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+
+      <p>
+        Welcome to Paradise Nursery, where green meets serenity!
+      </p>
+
+      <p>
+        At Paradise Nursery, we are passionate about bringing nature closer
+        to you. Our mission is to provide a wide range of high-quality plants
+        that enhance the beauty of your surroundings and contribute to a
+        healthier and more sustainable lifestyle.
+      </p>
+
+      <p>
+        From air-purifying plants to aromatic and fragrant plants, we have
+        something for every plant enthusiast. Our team is dedicated to
+        ensuring that every plant meets our standards of quality and care.
+      </p>
+
+      <p>
+        Whether you are an experienced gardener or just beginning your green
+        journey, Paradise Nursery is here to help you find the perfect plant
+        for your home or office.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
